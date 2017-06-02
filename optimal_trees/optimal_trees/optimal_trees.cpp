19d71da2827687557782ce814ae18e88f1f40de8
Сергей <sergey-global@yandex.ru>
2017-06-02 22:14:11 +0300

    Фикс кодировки

--- optimal_trees/optimal_trees/optimal_trees.cpp
+++ optimal_trees/optimal_trees/optimal_trees.cpp
@@ -11,10 +11,10 @@
 
 using namespace std;
 
-bool isCompatible(vector<node>, size_t, size_t);  //пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ i-пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+bool isCompatible(vector<node>, size_t, size_t);  //поиск всех своместимых для i-того элемента
 vector<node> findAllCompatibles(vector<node>, size_t);
 node makeParent(node*, node*);
-size_t min_node(vector<node>,bool);
+size_t min_node(vector<node>, bool);
 void erase(vector<node>*, size_t);
 
 int main()
@@ -31,54 +31,54 @@ int main()
 		{ 8, 'j', 8, 0, 0, 0 },
 		{ 9, 'k', 1, 0, 0, 0 },
 		{ 10, 'l', 3 ,0 ,0 ,0 }
-	} };
+		} };
 
 	node * root_first_tree = NULL;
-	
-	///////////пїЅпїЅпїЅ 1/////////////
-	// пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ (A, B)
+
+	///////////ШАГ 1/////////////
+	// ищем локально минимальную совместимую пару (A, B)
 	size_t  i, j;
 	size_t size = arr.size();
 	while (size - 1)
 	{
-		for (i = 0; i < size; i++) //пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
+		for (i = 0; i < size; i++) //перебор первого члена пары
 		{
-			// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ i-пїЅпїЅпїЅ
+			// фиксируем i-тый
 
-			// пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ.пїЅ.пїЅ.пїЅ.
-			// пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ i-пїЅпїЅпїЅ
+			// первая часть первого условия л.м.с.п.
+			// ищем все совместимые с i-тым
 			vector<node> compatibleWithI = findAllCompatibles(arr, i);
-			j = min_node(compatibleWithI,false);
+			j = min_node(compatibleWithI, false);
 			vector<node> compatibleWithJ = findAllCompatibles(arr, j);
-			size_t minCompatibleWithJ = min_node(compatibleWithJ,true);
+			size_t minCompatibleWithJ = min_node(compatibleWithJ, true);
 
-			if ((minCompatibleWithJ == i) && (i != j)) 
+			if ((minCompatibleWithJ == i) && (i != j))
 				goto all_found;
-				//break; break;		
+			//break; break;		
 		}
-		all_found:
-		//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ.пїЅ.пїЅ.пїЅ., пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
-		//пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+	all_found:
+		//комбинируем л.м.с.п., первый член заменятся отцом, второй уходит
+		//если корень пустой, кладем отца в корень
 		node a = arr[i];
 		node b = arr[j];
 		arr[i] = makeParent(&a, &b);
 		erase(&arr, j);
 		size--;
 	}
-	//пїЅпїЅ пїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ arr пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+	//ну и весь шаг надо зациклить пока в arr не останется один элемент
 
-	// пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+	// дальше хуета
 
-	////////////пїЅпїЅпїЅ 2///////////////////
-	// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-	stack<node> st;  
+	////////////ШАГ 2///////////////////
+	// стековый алгоритм
+	stack<node> st;
 	stack<node> qu;
-	for (int i = 10; i < 0; i--)  //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+	for (int i = 10; i < 0; i--)  //инициализация стека по имени очередь
 		qu.push(arr[i]);
-	
-	while (Move1(st, qu));   //пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-		
-    return 0;
+
+	while (Move1(st, qu));   //трабл с передачей по указателю
+
+	return 0;
 }
 
 size_t min_node(vector<node> all, bool orEqual) {
@@ -96,12 +96,12 @@ size_t min_node(vector<node> all, bool orEqual) {
 }
 
 void erase(vector<node> *nodes, size_t id) {
-	node rght = *(*nodes)[id - 1].right;   //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+	node rght = *(*nodes)[id - 1].right;   //костылиииииии
 	nodes->erase(nodes->begin() + id);
 	(*nodes)[id - 1].right = &rght;
 	for (size_t i = id; i < nodes->size(); i++)
 	{
-		--(*nodes)[i].id;           //пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ id пїЅпїЅпїЅпїЅпїЅ?
+		--(*nodes)[i].id;           //что делать с id детей?
 	}
 }
 
@@ -125,18 +125,18 @@ node makeParent(node *a, node *b) {
 	c.right = b;
 	node * zh = &c;
 	return c;*/
-	return { a->id, '\0', (a->weight+b->weight), 0, a, b };
+	return{ a->id, '\0', (a->weight + b->weight), 0, a, b };
 }
 
 vector<node> findAllCompatibles(vector<node> arr, size_t curr)
-{   
+{
 	vector<node> all_compatible;
 	for (size_t i = 0; i < arr.size(); i++)
 		if (i != curr && isCompatible(arr, curr, i))
 		{
 			all_compatible.push_back(arr[i]);
 		}
-	return all_compatible;  
+	return all_compatible;
 }
 
 
@@ -147,8 +147,7 @@ bool isCompatible(vector<node> arr, size_t a, size_t b) {
 	if (a > b) swap(a, b);
 	for (size_t i = a + 1; i < b; i++)
 	{
-		if (arr[i].left==nullptr) return false;
+		if (arr[i].left == nullptr) return false;
 	}
 	return true;
 }
-
