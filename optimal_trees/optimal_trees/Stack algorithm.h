884074d66b1f83b7fc625aee778b9912593a9785
Unknown <sergey-global@yandex.ru>
2017-06-02 04:13:32 +0300

    Почти готовый первый шаг Ху-Таккера
    TODO: починить наследование детей

--- optimal_trees/optimal_trees/Stack algorithm.h
+++ optimal_trees/optimal_trees/Stack algorithm.h
@@ -5,9 +5,44 @@
 using namespace std;
 
 struct node {
+	
+	size_t id;
 	char sign;
-	int weight, level;
-	node *left, *right;
+	size_t weight, level;
+	node* left; 
+	node* right;
+	node() {
+		id = NULL;
+		sign = 0;
+		weight = 0;
+		level = 0;
+		left = nullptr;
+		right = nullptr;
+	}
+	node(char s, size_t w, size_t l, node *lt, node *rt) {
+		id = NULL;
+		sign = s;
+		weight = w;
+		level = l;
+		left = lt;
+		right = rt; 
+	}
+	node(size_t i, char s, size_t w, size_t l, node *lt, node *rt) {
+		id = i;
+		sign = s;
+		weight = w;
+		level = l;
+		left = lt;
+		right = rt;
+	}
+	node(const node&Ob) {
+		id = Ob.id;
+		sign = Ob.sign;
+		weight = Ob.weight;
+		level = Ob.level;
+		left = Ob.left;
+		right = Ob.right;
+	}
 };
 
 
