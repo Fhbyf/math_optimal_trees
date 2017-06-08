df50192dce26d8923ffcddfeb1f75b5ae064656f
Арина <@LAPTOP-RDGC957J>
2017-06-08 20:45:58 +0300

    Дописан стековый алгоритм

--- optimal_trees/optimal_trees/Stack algorithm.cpp
+++ optimal_trees/optimal_trees/Stack algorithm.cpp
@@ -7,37 +7,32 @@
 
 using namespace std;
 
-void Move2(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i, size_t max_level)
+void Move2(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i)
 {
 	st.push(qu.top());
 	qu.pop();
-	Move1(st, qu, parents, parents_i, max_level);
+	Move1(st, qu, parents, parents_i);
 }
 
-bool Move3(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i, size_t max_level)
+bool Move3(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i)
 {
 	//строим дерево по принципу: два верхних элемента стека st располагаем на соответствующих уровнях
-	//делаем что-то похожее на //комбинируем л.м.с.п., первый член ...
 	//помещаем отца на уровень детей - 1, делаем связи между ними, очевидно
-	
-	//{
-		node* top1 = st.top();
-		st.pop();
-		node* top2 = st.top();
-		st.pop();
-		//что с символом делать
-		parents[parents_i] = makeParent(top2, top1, false);
-		st.push(&parents[parents_i]);
-		++parents_i;
-		if (st.top()->level) return 1;
-	//}
+	node* top1 = st.top();
+	st.pop();
+	node* top2 = st.top();
+	st.pop();
+	parents[parents_i] = makeParent(top2, top1, false);
+	st.push(&parents[parents_i]);
+	++parents_i;
+	if (st.top()->level) return 1;
 	return 0;
 }
 
-bool Move1(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i, size_t max_level)
+bool Move1(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i)
 {
 	if (st.size() < 2)
-		Move2(st, qu, parents, parents_i, max_level);
+		Move2(st, qu, parents, parents_i);
 	else
 	{
 		node* top1 = st.top();
@@ -45,23 +40,18 @@ bool Move1(stack<node*> &st, stack<node*> &qu, node*parents, size_t& parents_i, 
 		node* top2 = st.top();
 		st.push(top1);
 		if (top1->level == top2->level)
-			return Move3(st, qu, parents, parents_i, max_level);
+			return Move3(st, qu, parents, parents_i);
 		else
-			Move2(st, qu, parents, parents_i, max_level);
+			Move2(st, qu, parents, parents_i);
 	}
 }
 
 node* buildTree(vector<node>& nodes, node* parents) {
 	stack<node*> st;
 	stack<node*> qu;
-	size_t max_level = 0;
 	for (int i = nodes.size() - 1; i >= 0; i--)  //инициализация стека по имени очередь
-	{
-		if (nodes[i].level > max_level)
-			max_level = nodes[i].level;
 		qu.push(&nodes[i]);
-	}
 	size_t parents_i = 0;
-	while (Move1(st, qu, parents, parents_i, max_level));
+	while (Move1(st, qu, parents, parents_i));
 	return st.top();
 }
